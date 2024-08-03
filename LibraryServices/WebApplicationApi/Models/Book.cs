0fcdd5487ae5297eb85f13cd5d65a5453296c38e
Ade Cuellar <76180286+adecuellarv@users.noreply.github.com>
2024-08-03 11:40:09 -0600

    models

---
+++ LibraryServices/WebApplicationApi/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplicationApi.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string BookGuid { get; set; }
+        public string BookName { get; set; }
+        public string BookDescription { get; set; }
+        public string BookImage {get; set; }
+        public string BookPdf { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ LibraryServices/WebApplicationApi/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplicationApi.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryGuid { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryImage { get; set; }
+        public ICollection<Book> Books { get; set;}
+    }
+}
