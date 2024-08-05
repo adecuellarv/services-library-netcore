0adc3c75e60b1fd8b77f1b242871a65cc584dc42
Ade Cuellar <76180286+adecuellarv@users.noreply.github.com>
2024-08-05 01:55:28 -0600

    update books

--- LibraryServices/WebApplicationApi/Controllers/BookController.cs
+++ LibraryServices/WebApplicationApi/Controllers/BookController.cs
@@ -62,8 +62,33 @@ namespace WebApplicationApi.Controllers
 
         // PUT api/<BookController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<ActionResult<Unit>> Update([FromForm] EditBook.ExecuteBook data, string id)
         {
+            if (data.BookImageFile == null || data.BookPdfFile == null)
+                return BadRequest("Los archivos no pueden ser nulos.");
+
+            // path
+            var directoryPath = @"C:\Users\Usuario\Documents\projects\files-exam";
+            var bookImagePath = Path.Combine(directoryPath, data.BookImageFile.FileName);
+            var bookPdfPath = Path.Combine(directoryPath, data.BookPdfFile.FileName);
+
+            //upload
+            using (var stream = new FileStream(bookImagePath, FileMode.Create))
+            {
+                await data.BookImageFile.CopyToAsync(stream);
+            }
+
+            using (var stream = new FileStream(bookPdfPath, FileMode.Create))
+            {
+                await data.BookPdfFile.CopyToAsync(stream);
+            }
+
+            // path to bd
+            data.BookImage = bookImagePath;
+            data.BookPdf = bookPdfPath;
+            data.BookId = int.Parse(id);
+
+            return await _mediator.Send(data);
         }
 
         // DELETE api/<BookController>/5
